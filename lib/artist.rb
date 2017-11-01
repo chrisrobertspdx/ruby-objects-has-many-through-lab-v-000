a631500037b72f661a8254eba1b3befc233c5b94
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-01 17:50:03 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    self.songs.collect{|s|
+      s.genre
+    }

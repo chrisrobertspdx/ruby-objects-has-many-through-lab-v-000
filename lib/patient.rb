c8d32deac789270a6927a3950f71141b6a796eb6
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-01 18:53:53 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,11 @@
+class Patient
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = patient
+  end
+end
